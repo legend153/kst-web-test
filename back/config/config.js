c5f9961119754fcac51fd5955566b047a4e29362
st <st@bejewel.kr>
2020-08-07 22:26:49 +0900

    back start script

--- back/config/config.js
+++ back/config/config.js
@@ -6,22 +6,22 @@ module.exports = {
   development: {
     username: 'root',
     password: process.env.DB_PASSWORD,
-    database: 'kst_test',
-    host: "testdb.amondz.com",
+    database: 'react-nodebird',
+    host: '127.0.0.1',
     dialect: 'mysql',
   },
   test: {
     username: 'root',
     password: process.env.DB_PASSWORD,
-    database: 'kst_test',
-    host: "testdb.amondz.com",
+    database: 'react-nodebird',
+    host: '127.0.0.1',
     dialect: 'mysql',
   },
   production: {
     username: 'root',
     password: process.env.DB_PASSWORD,
-    database: 'kst_test',
-    host: "testdb.amondz.com",
+    database: 'react-nodebird',
+    host: '127.0.0.1',
     dialect: 'mysql',
   },
 };
